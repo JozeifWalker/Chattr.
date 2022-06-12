c415790f42cc44610df097815720190ddd6f69aa
Jozeif Walker <gp1891@gmail.com>
2022-06-12 00:33:25 -0500

    landing page style refactoring

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import Logo from '../../components/logo/logo';
 import './landing.scss';
+import Stack from 'react-bootstrap/esm/Stack';
 
 const Landing = () => {
 	return (
 		<React.Fragment>
 			<div className="landing-container">
 				<Container fluid id="landing-contents">
-					<h1 id='site-title'>Chattr.</h1>
+					<Stack style={{alignItems:'center',justifyContent:'center'}} gap={5}>
+					<Logo />
+					{/* <h1 id='site-title'>Chattr.</h1> */}
                     <h3>Taking collaboration & teamwork to the next level.</h3>
                     <Button href="/login" id={'signIn'}>Sign In</Button>
                     <hr style={{backgroundColor:'white',width:'50%',margin:'2rem 0rem'}}></hr>
                     <p>Dont have an account ? <Link to="/">Create one.</Link></p>
+					</Stack>
                     
 				</Container>
 			</div>
